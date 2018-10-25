c30e5240f22686e54fd2ea0ea367b64be01554c2
Jacilyn Hayden <jacihayden@gmail.com>
2018-10-25 21:11:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solutions in this file
+function printBadges(name) {
+    for (let i = 0; i < name.length; i++) {
+        console.log(`Welcome ${name[i]}! You are employee #(${i}+1).`);
+    }
+
+    return name;
+}
